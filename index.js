f5dd2f97c1ff18fea77d88dfda8b12b3a0536072
shakurShirajul <siirajulisllam@gmail.com>
2024-08-17 09:22:59 +0600

    models fixed

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import 'dotenv/config';
 import { database } from './database/mongodb.js';
 
 // Schema
-import { Products } from './models/products';
+import { Products } from './models/products.js';
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -14,11 +14,19 @@ database();
 
 // MiddleWares
 app.use(express.json());
+app.use(cors());
 app.use(cookieParser());
 
 app.get("/", async(req,res)=>{
     res.send("Hello World");
 });
+
+
+// Products Routes
+app.get("/products", async(req,res)=>{
+    const products = await Products.find({});
+    res.send(products)
+})
 
 app.listen(PORT, ()=>{
     console.log(`Server running on ${PORT}` );
--- models/products.js
+++ models/products.js
@@ -1,37 +1,40 @@
 import mongoose from "mongoose";
 
 const productSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true,
-    },
-    brand:{
-        type: String,
-        required: true,
-    },
-    image:{
-        type: String,
-        required: true,
-    },
-    price:{
-        type: Number,
-        requrired: true,
-    },
-    category:{
-        type: String,
-        required: true, 
-    },
-    rating:{
-        type: Number,
-        requrired: 0,
-    },
-    date:{
-        type: Date,
-        default: Date.now,
-    },
-    description:{
-        type: String,
-        requrired: true,
-    }
-})
-export const Products = mongoose.model("products", productSchema)
+  name: {
+    type: String,
+    required: true,
+  },
+  brand: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    requrired: true,
+  },
+  category: {
+    type: String,
+    required: true,
+  },
+  rating: {
+    type: Number,
+    requrired: 0,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+  description: {
+    type: String,
+    requrired: true,
+  },
+});
+
+productSchema.index({ name: "text" }, { default_language: "none" });
+
+export const Products = mongoose.model("products", productSchema);
